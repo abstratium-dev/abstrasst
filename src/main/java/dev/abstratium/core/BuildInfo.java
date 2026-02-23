590d4fa94ffeed0ca43302f81833cc8310acb1d4
Ant Kutschera <1108507+maxant@users.noreply.github.com>
2026-02-23 21:50:28 +0100

    adding footer

--- src/main/java/dev/abstratium/core/BuildInfo.java
+++ src/main/java/dev/abstratium/core/BuildInfo.java
@@ -18,7 +18,7 @@ public final class BuildInfo {
      * This value is automatically updated by .git/hooks/pre-commit
      * DO NOT EDIT MANUALLY - changes will be overwritten
      */
-    public static final String BUILD_TIMESTAMP = "2026-02-23T20:46:07Z";
+    public static final String BUILD_TIMESTAMP = "2026-02-23T20:50:28Z";
     
     private BuildInfo() {
         // Utility class - prevent instantiation
